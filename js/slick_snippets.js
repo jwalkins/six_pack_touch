5f5eb8b940ff86fbe99fcaac71e291d27c1b15ab
jwalkins <james.walkinshaw@me.com>
2017-03-09 22:40:41 +0000

    Lots of additions

---
+++ js/slick_snippets.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.carousel-photos').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+});
+
+		
